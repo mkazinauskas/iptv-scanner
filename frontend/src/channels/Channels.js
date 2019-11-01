328172bda50a160d68ae4ab5852d03300a4d8610
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2019-11-02 00:46:09 +0200

    UI channels download

--- frontend/src/channels/Channels.js
+++ frontend/src/channels/Channels.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
     Container,
     Header,
+    Button
 } from 'semantic-ui-react';
 
 import ChannelsTable from './Table'
@@ -11,6 +12,10 @@ class Channels extends React.Component {
         return (
             <Container style={{ marginTop: '7em' }}>
                 <Header as='h1'>Current channels</Header>
+                <div style={{ marginBottom: '0.5em' }}>
+                    <Button class="ui button" href="http://localhost:8080/channels/export?size=1000&status=WORKING" target="_blank">Download Working channels</Button>
+                    <Button class="ui button" href="http://localhost:8080/channels/export?size=1000" target="_blank">Download All channels</Button>
+                </div>
                 <div><ChannelsTable /></div>
             </Container >
         );
