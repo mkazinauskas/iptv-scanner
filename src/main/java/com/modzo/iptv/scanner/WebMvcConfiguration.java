8561513bdc8d5cc1ec95d4459eeca7b604efbb11
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2019-11-05 00:07:22 +0200

    Removed not required code

--- src/main/java/com/modzo/iptv/scanner/WebMvcConfiguration.java
+++ src/main/java/com/modzo/iptv/scanner/WebMvcConfiguration.java
@@ -10,15 +10,8 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
-    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-            "classpath:/META-INF/resources/", "classpath:/resources/",
-            "classpath:/static/", "classpath:/public/" };
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
-
         registry.addResourceHandler("/gui/**")
                 .addResourceLocations("classpath:/gui/")
                 .resourceChain(true);
